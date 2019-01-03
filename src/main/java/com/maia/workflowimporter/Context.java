6b538ce87b458635c9d2965e331780520f05d658
leandrocmaia <leandrocm87@gmail.com>
2019-01-03 20:01:10 +0100

    fixed main context

--- src/main/java/com/maia/workflowimporter/Context.java
+++ src/main/java/com/maia/workflowimporter/Context.java
@@ -4,34 +4,28 @@ import com.maia.workflowimporter.migrator.ContractorMigrator;
 import com.maia.workflowimporter.migrator.EmployeeMigrator;
 import com.maia.workflowimporter.migrator.WorkflowInstanceMigrator;
 import com.maia.workflowimporter.migrator.WorkflowMigrator;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
-import java.io.IOException;
 
 public class Context {
 
 	public static void main(String[] args) {
-		try {
 
-			// application context provides data source for the migrators, could be local files or remote. Migrators are agnostic to source.
-			File contractorsData = new ClassPathResource("contractors.data", Context.class.getClassLoader()).getFile();
-			File employeesData = new ClassPathResource("employees.data", Context.class.getClassLoader()).getFile();
-			File workflowsData = new ClassPathResource("workflows.data", Context.class.getClassLoader()).getFile();
-			File workflowInstancesData = new ClassPathResource("workflowInstances.data", Context.class.getClassLoader()).getFile();
+        // application context provides data source for the migrators, could be local files or remote. Migrators are agnostic to source.
+        File contractorsData = new File("src/main/resources/contractors.data");
+        File employeesData = new File("src/main/resources/employees.data");
+        File workflowsData = new File("src/main/resources/workflows.data");
+        File workflowInstancesData = new File("src/main/resources/workflowInstances.data");
 
-			WorkflowInstanceMigrator migrator = new WorkflowInstanceMigrator(
-					workflowInstancesData,
-					new EmployeeMigrator(employeesData),
-					new ContractorMigrator(contractorsData),
-					new WorkflowMigrator(workflowsData));
+        WorkflowInstanceMigrator migrator = new WorkflowInstanceMigrator(
+                workflowInstancesData,
+                new EmployeeMigrator(employeesData),
+                new ContractorMigrator(contractorsData),
+                new WorkflowMigrator(workflowsData));
 
-			migrator.logSummary();
+        migrator.logSummary();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
 }
 
